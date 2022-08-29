89a40709b974e4c5dbf7099f8ed3b3022c84925b
Matias Erazo <micropio2046@gmail.com>
2022-08-29 01:56:54 -0300

    terminado 1

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Navbar from './NabVar/Navbar';
+import ItemListContainer from './ItemListContainer/ItemListContainer';
+
+function App() {
+  return (
+    <div id='' className="App">
+      <Navbar />
+      <ItemListContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Counter from './Counter';
+
+const ItemListContainer = () => {
+    return (
+        <>
+            <div className="card text-white bg-danger mb-3" style={{ maxWidth: '20rem' }}>
+                <div className="card-header">Almohadon Mostaza</div>
+                <div className="card-body">
+                </div>
+                < Counter stock={5}/>
+            </div>
+        </>
+    );
+}
+
+export default ItemListContainer;
+
